e887f368daf232a21112303ab07f1099ac49ae82
MohitAnand2 <anand2003mohit@gmail.com>
2025-02-01 01:05:26 +0530

    fix: Updated model

--- faqs/models.py
+++ faqs/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from googletrans import Translator
 
-translator = Translator()
-
 class FAQ(models.Model):
     question = models.TextField()
     answer = models.TextField()
@@ -10,8 +8,18 @@ class FAQ(models.Model):
     question_bn = models.TextField(blank=True, null=True)  # Bengali translation
 
     def save(self, *args, **kwargs):
-        if not self.question_hi:
-            self.question_hi = translator.translate(self.question, src='en', dest='hi').text
-        if not self.question_bn:
-            self.question_bn = translator.translate(self.question, src='en', dest='bn').text
+        translator = Translator()
+
+        try:
+            if not self.question_hi and self.question:
+                translated_hi = translator.translate(self.question, src="en", dest="hi")
+                self.question_hi = translated_hi.text  # ✅ FIXED: No async issue
+
+            if not self.question_bn and self.question:
+                translated_bn = translator.translate(self.question, src="en", dest="bn")
+                self.question_bn = translated_bn.text  # ✅ FIXED: No async issue
+
+        except Exception as e:
+            print(f"Translation error: {e}")  # Logs error but allows saving
+
         super().save(*args, **kwargs)
